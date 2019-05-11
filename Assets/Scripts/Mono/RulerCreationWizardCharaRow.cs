31d91338be733d682c775a94ddb1b3beae1af3c8
rackover <rackover@racknet.noip.me>
2019-05-11 14:20:09 +0200

    Imported font and redesigned character sheet layout

---
+++ Assets/Scripts/Mono/RulerCreationWizardCharaRow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class RulerCreationWizardCharaRow : MonoBehaviour
+{
+    public TextMeshProUGUI valueText;
+    public TextMeshProUGUI nameText;
+    public Button increaseButton;
+    public Button decreaseButton;
+    public Ruler.Characteristic characteristic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
