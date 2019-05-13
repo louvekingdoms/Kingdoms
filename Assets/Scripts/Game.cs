3e186e25ff40802deb6b65bb26831517f55d7f0c
rackover <rackover@racknet.noip.me>
2019-05-13 09:24:36 +0200

    Added Game.CS with main thread ID storage

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public static class Game
+{
+    static Thread mainThread = Thread.CurrentThread;
+
+    public static bool IsRunningInMainThread()
+    {
+        return Thread.CurrentThread == mainThread;
+    }
+}
