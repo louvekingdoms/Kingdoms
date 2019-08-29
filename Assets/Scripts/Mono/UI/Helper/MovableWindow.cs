4d29f55e9fcb491ab1dd8ed1016182dfb8bfedca
louve <rackover@racknet.noip.me>
2019-08-30 01:26:09 +0530

    Added debug windows

---
+++ Assets/Scripts/Mono/UI/Helper/MovableWindow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MovableWindow : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public RectTransform window;
+
+    Vector2 dragStartPosition;
+    bool isDragging;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isDragging = true;
+        dragStartPosition = window.anchoredPosition - new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isDragging = false;
+    }
+
+    private void Update()
+    {
+        if (isDragging)
+        {
+            window.anchoredPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y) + dragStartPosition;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Mono/UI/Helper/ResizeableWindow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ResizeableWindow : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public RectTransform window;
+    public UnityEngine.UI.Image handleImage;
+    public UnityEngine.UI.Image titleBar;
+
+    float dragStartPosition;
+    float startWidth;
+    bool isDragging;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isDragging = true;
+        dragStartPosition = Input.mousePosition.x;
+        startWidth = window.sizeDelta.x;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isDragging = false;
+    }
+
+    private void Update()
+    {
+        handleImage.color = titleBar.color;
+
+        if (isDragging)
+        {
+            window.sizeDelta = new Vector2(startWidth + Input.mousePosition.x - dragStartPosition, window.sizeDelta.y);
+        }
+    }
+}
